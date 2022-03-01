053a069714dcab38a685ba9fc1b20a8bb63a562c
Lachlan Trembath <96830517+blt-code@users.noreply.github.com>
2022-03-01 23:53:01 +0000

    Create Quiz Show: The Numbers Round.py
    On the quiz show "Letters and Numbers" there is a round in which contestants are given six ingredient numbers and one target number, and their challenge is to apply arithmetic operations (addition, subtraction, multiplication, division) to one or more of the ingredient numbers to get the target number.
    The ingredient numbers are chosen randomly as follows: 0-4 large numbers, chosen from {25, 50, 75, 100} with no repeats and the remaining are all small numbers, chosen from {1, 2, 3, ..., 10} with no repeats.
    Each ingredient number can only be used once, but they need not all be used. No fractions are allowed at any stage of the calculation.
    The target number is also chosen randomly from 100 to 1000.

---
+++ Quiz Show: The Numbers Round.py
@@ -0,0 +1,85 @@
+### UNFINISHED
+
+import random
+import itertools
+
+# Generate our target and our ingredients 
+
+target = random.randint(100,1000)
+print(f'Target: {target}')
+
+large_nums = [25,50,75,100]
+small_nums = [1,2,3,4,5,6,7,8,9,10]
+
+n_large = random.randint(0,4)
+n_small = 6-n_large
+
+large_ings = []
+small_ings = []
+
+i = 0
+while i < n_large:
+    pick = random.choice(large_nums)
+    large_nums.remove(pick)
+    large_ings.append(pick)
+    i+=1
+
+i = 0
+while i < n_small:
+    pick = random.choice(small_nums)
+    small_nums.remove(pick)
+    small_ings.append(pick)
+    i+=1
+
+ings = large_ings + small_ings
+print(f'Ingredients: {ings}')
+
+
+### HERE DOWN IS UNFINISHED
+# Gather operation permutations with repitition
+
+operations = ('*','+','/','-')
+
+j = 1
+opPerms = []
+while j < 6:
+    for i in itertools.product(operations, repeat = j):
+        opPerms.append(i)
+    j += 1
+
+# Gather ingredients permutations without repitition
+
+j = 2
+numPerms = []
+while j <= 6:
+    for i in itertools.permutations(ings, j):
+        numPerms.append(i)
+    j += 1
+
+# Weave together ingredients and operations permutations
+
+perms = []
+for nums in numPerms:
+    for ops in opPerms:
+        if len(nums) == len(ops)+1:
+            nums = list(nums)
+            ops = list(ops)
+            ops.extend("E")
+            lst = list(itertools.chain.from_iterable(zip(nums, ops)))[:-1] 
+            perms.append(lst)
+
+# Convert permutation lists to expression strings
+
+expressions = []
+for perm in perms:
+    expressions.append(' '.join(str(item) for item in perm))
+
+# Evaluate each expression and compare to the target
+
+solutions = 0
+for exp in expressions: 
+    if eval(exp) == target:
+        solutions += 1
+        print(f"Possible answer: {exp} = {target}")
+print(f"\n{solutions} answers found\n")
+
